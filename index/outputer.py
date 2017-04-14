8515f8d61fd28a46c50badc3f3792f8b364659cc
study_sun <msun@gongpingjia.com>
2017-04-14 19:08:25 +0800

    implement print version index value percent

--- index/outputer.py
+++ index/outputer.py
@@ -40,34 +40,38 @@ class IndexOutputer(object):
     def __init__(self):
         self.flat_policy = 0
 
+    #对数组的数组取中位数,自然不需要什么抹平
+    def _median(self, data):
+        mids = []
+        for one_list in data:
+            one_list = sorted(one_list)
+            mids.append(median(one_list))
+        return mids
+
     # 暂时不知道什么策略,先留白吧,pe和pb可能策略也不一样,所以写了两个函数
-    def _flat_pe(self, pes, flat_policy = StockDataFlatPolicy.RAW):
+    def _flat(self, pes, flat_policy = StockDataFlatPolicy.RAW.value):
         pes = to_container(pes)
-        if flat_policy == StockDataFlatPolicy.RAW:
-            return pes
-        for (index, value) in enumerate(pes):
-            to_remove = []
-            if value < 0:
-                if flat_policy & StockDataFlatPolicy.DISCARD_NEGATIVE > 0:
-                    to_remove.append(value)
-                elif flat_policy & StockDataFlatPolicy.ZERO_NEGATIVE > 0:
-                    pes[index] = 0
-            elif value > 100:
-                if flat_policy & StockDataFlatPolicy.DISCARD_LARGE > 0:
-                    to_remove.append(value)
-        pes.remove()
+        if flat_policy != StockDataFlatPolicy.RAW.value:
+            for (index, value) in enumerate(pes):
+                if flat_policy & StockDataFlatPolicy.DISCARD_NEGATIVE.value:
+                    value = [i for i in value if i >= 0]
+                if flat_policy & StockDataFlatPolicy.DISCARD_LARGE.value:
+                    value = [i for i in value if i <= 100]
+                if flat_policy & StockDataFlatPolicy.ZERO_NEGATIVE.value:
+                    value = [max(i, 0) for i in value]
+                pes[index] = value
+        flat_pes = []
+        for value in pes:
+            flat_pes.append(sum(value)/len(value))
+        return flat_pes
 
-
-    def _flat_pb(selfself, pbs, flat_policy = StockDataFlatPolicy.RAW):
+    def _flat_pb(selfself, pbs, flat_policy = StockDataFlatPolicy.RAW.value):
         pass
 
     # 纯粹按数据份数来百分比值,可能以后会有按年份分的
     # 数值请尽量多一些,不然我不好10%的取
     def _segment(self, data):
-        sumed = []
-        for one in data:
-            sumed.append(sum(one)/len(one))
-        sumed.sort()
+        sumed = sorted(data)
         length = len(sumed)
         r = range(0, 10)
         s = [sumed[int(float(length)/10*i)] for i in r]
@@ -79,7 +83,7 @@ class IndexOutputer(object):
     # 数据时间段xxx天
     # 0%pe为9.2, 10%pe为10.2, 20%pe威武12, ... 100%pe为30
     # 0#pb为xx...
-    def print_index_quotations(self, index_quotations, flat_policy=StockDataFlatPolicy.NORMAL, average_policy=StockDataAveragePolicy.MEAN,
+    def print_index_quotations(self, index_quotations, flat_policy=StockDataFlatPolicy.NORMAL.value, average_policy=StockDataAveragePolicy.MEAN.value,
                                today_pes=[], today_pbs=[]):
         index_quotations = to_container(index_quotations)
         for index_quotation in index_quotations:
@@ -90,21 +94,22 @@ class IndexOutputer(object):
             for index_day_quotation in index_quotation[1]:
                 pes.append(index_day_quotation[1])
                 pbs.append(index_day_quotation[2])
+            # 此时的pes和pbs是数组的数组,且未经处理
+            # 请注意中位数不是指把他们加起来之后的中位数,而是每天的中位数哦
+            mid_pes = self._median(pes)
+            mid_pbs = self._median(pbs)
+            # 再根据输出策略进行数据抹平,才好算平均数,pb就不抹平了吧
+            flat_pes = self._flat(pes, flat_policy)
+            flat_pbs = self._flat(pbs, StockDataFlatPolicy.RAW.value)
+            mid_segmented_pes = self._segment(mid_pes)
+            mid_segmented_pbs = self._segment(mid_pbs)
+            flat_segmented_pes = self._segment(flat_pes)
+            flat_segmented_pbs = self._segment(flat_pbs)
 
-            pes = sorted(pes)
-            pbs = sorted(pbs)
-            # 中位数无需进行数据抹平,排个序就好了
-            mid_pe = median(pes)
-            mid_pb = median(pbs)
-            # 此时根据输出策略进行数据抹平,才好算平均数
-            self._flat_pe(pes, flat_policy)
-            self._flat_pb(pbs, flat_policy)
-            segmented_pes = self._segment(pes)
-            segmented_pbs = self._segment(pbs)
-
-            print 'pe 百分点位为 ' + ','.join([str(i) for i in segmented_pes])
-            print 'pb 百分点位为 ' + ','.join([str(i) for i in segmented_pbs])
-
+            print 'pe 平均数百分点位为 ' + ','.join([str(i) for i in flat_segmented_pes])
+            print 'pe 中位数百分点位为 ' + ','.join([str(i) for i in mid_segmented_pes])
+            print 'pb 平均数百分点位为 ' + ','.join([str(i) for i in flat_segmented_pbs])
+            print 'pb 中位数百分点位为 ' + ','.join([str(i) for i in mid_segmented_pbs])
             # 最后应该再来个当天处于什么百分位比较好,就以最后一个行情为当天吧
 
 
@@ -118,12 +123,3 @@ class IndexOutputer(object):
 
 if __name__ == '__main__':
     a = IndexOutputer()
-    # a.xixi(b=2,s=1,c='zz',d='hehe')
-    l = [1,2,3,-1,-3,1,2,-9]
-    z = StockDataFlatPolicy.NORMAL.value
-
-    if z & StockDataFlatPolicy.ZERO_NEGATIVE.value > 0:
-        x = [max(0, i) for i in l]
-    if z & StockDataFlatPolicy.DISCARD_NEGATIVE.value > 0:
-        x = [i for i in l if i > 0]
-    print x
