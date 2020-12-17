83679f901699ea2af2e9f65f95c3052d5a992fc5
study_sun <s6530085@hotmail.com>
2020-12-17 17:49:51 +0800

    series all

--- Fund/series.py
+++ Fund/series.py
@@ -51,41 +51,45 @@ class Series(object):
 
         return fundDict
 
-    def series(self, file, fundDict, sheetname):
+    def series(self, file, fundDict, sheetname, forall):
         flows = file.sheet_by_name(sheetname)
-        allMoney = 0.0
-        allStock = 0.0
-        allBond = 0.0
-        allCash = 0.0
-        allCommodity = 0.0
-        for i in range(1, flows.nrows):
-            code = flows.cell(i, 0).value
-            if len(code) > 0:
-                fundInfo = fundDict.get(code, 0)
-                if fundInfo == 0:
-                    print '{} 没找到对应基金信息, 名称为{}'.format(code, flows.cell(i, 1).value)
-                    # 没信息就是股票
-                    stock = flows.cell(i, flows.ncols - 1).value
-                    if not isinstance(stock, unicode):
-                        allStock += stock
-                        allMoney += stock
-                    continue
-                money = flows.cell(i, flows.ncols - 1).value
-                if isinstance(money, unicode):
-                    continue
-                stock = money * fundInfo['stock']
-                bond = money * fundInfo['bond']
-                cash = money * fundInfo['cash']
-                commodity = money * fundInfo['commodity']
-                allMoney += money
-                allStock += stock
-                allBond += bond
-                allCash += cash
-                allCommodity += commodity
-        print '{}表中账户共有{}, 股票占比{:.2f}, 债券占比{:.2f}, 现金占比{:.2f}, 商品占比{:.2f}'.format(sheetname, allMoney, allStock / allMoney,
-                                                                              allBond / allMoney,
-                                                                              allCash / allMoney,
-                                                                              allCommodity / allMoney)
+        startCol = 2 if forall else flows.ncols - 1
+
+        for j in range(startCol, flows.ncols):
+            timeDate =  xlrd.xldate_as_tuple(flows.cell(0,j).value, 0)
+            timeString = '{}/{}/{}'.format(timeDate[0], timeDate[1], timeDate[2])
+            allMoney = 0.0
+            allStock = 0.0
+            allBond = 0.0
+            allCash = 0.0
+            allCommodity = 0.0
+            for i in range(1, flows.nrows):
+                code = flows.cell(i, 0).value
+                if len(code) > 0:
+                    fundInfo = fundDict.get(code, 0)
+                    if fundInfo == 0:
+                        print '{} 没找到对应基金信息, 名称为{}'.format(code, flows.cell(i, 1).value)
+                        # 没信息就是股票
+                        stock = flows.cell(i, j).value
+                        if not isinstance(stock, unicode):
+                            allStock += stock
+                            allMoney += stock
+                        continue
+                    money = flows.cell(i, j).value
+                    if isinstance(money, unicode):
+                        continue
+                    stock = money * fundInfo['stock']
+                    bond = money * fundInfo['bond']
+                    cash = money * fundInfo['cash']
+                    commodity = money * fundInfo['commodity']
+                    allMoney += money
+                    allStock += stock
+                    allBond += bond
+                    allCash += cash
+                    allCommodity += commodity
+            print '{}中账户在{}共有{}, 股票占比{:.2f}%, 债券占比{:.2f}%, 现金占比{:.2f}%, 商品占比{:.2f}%'\
+                .format(sheetname, timeString, allMoney, 100 * allStock / allMoney, 100 * allBond / allMoney, 100 * allCash / allMoney, 100 * allCommodity / allMoney)
+
 
 
 if __name__ == "__main__":
@@ -94,5 +98,5 @@ if __name__ == "__main__":
     if file is not None:
         t = Series()
         fundDict = t.loadFile(file)
-        t.series(file, fundDict, '家庭账户时序表')
-        t.series(file, fundDict, '个人账户时序表')
+        t.series(file, fundDict, '家庭账户时序表', 1)
+        t.series(file, fundDict, '个人账户时序表', 0)
